b531444d82218e1fdb4e87cb1d812aff7064d82b
fops4 <fops415@gmail.com>
2024-11-13 01:44:21 +0100

    integration de electron

---
+++ main.js
@@ -0,0 +1,31 @@
+// main.js
+const { app, BrowserWindow } = require('electron');
+
+// Fonction pour créer une fenêtre de navigateur
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+    },
+  });
+
+  win.loadFile('index.html');
+}
+
+// Événements d'application
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
